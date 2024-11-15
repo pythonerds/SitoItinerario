f5ee6d486e78e4504a7af2fcd762d8ccbd98948c
Giogam3r05 <fiannaca2005@gmail.com x>
2024-11-15 11:05:42 +0100

    aggiunta codice per per pubblicare siti

--- SitoItinerario/SitoItinerario/settings.py
+++ SitoItinerario/SitoItinerario/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.utils.translation import gettext_lazy as _
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,10 +95,7 @@ WSGI_APPLICATION = 'SitoItinerario.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
