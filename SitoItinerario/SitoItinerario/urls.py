3268b2efd213143e9fb8eeba15ed1e2eee6a5352
Giovanni <135823200+Giogam3r05@users.noreply.github.com>
2025-02-07 23:03:37 +0100

    ultimi file

--- SitoItinerario/SitoItinerario/urls.py
+++ SitoItinerario/SitoItinerario/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from itinerarioPirandelliano.views import set_language
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
@@ -27,3 +28,6 @@ urlpatterns = i18n_patterns(
     path('', include('itinerarioPirandelliano.urls')),
    
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
